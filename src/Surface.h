88d48a18ee6bfd0dee4dcb85452dddc324080782
Benjamin Brock <bbrock4@utk.edu>
2014-01-04 13:53:32 -0500

    Minor changes.

--- src/Surface.h
+++ src/Surface.h
@@ -20,14 +20,13 @@ class Surface {
     double m; /* Maximum possible permutations. */
     std::vector <class Voxel *> voxels;
 
-    void connect(); /* Set adj pointers. */
     void mutate(); /* Mutate surface in accordance with p and m. */
+    void print();
 
     bool permute(Voxel *v); /* Attempt to permute Voxel to which v points. */
     bool check(); /* Check if surface is valid. */
+    void connect(); /* Set adj pointers. */
     bool exists(int x, int y, int z); /* Returns whether a voxel exists at x, y, z. */
-
-    void print();
 
     Surface(int n);
     ~Surface();
